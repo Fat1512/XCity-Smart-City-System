57cc8dc7c94789efe74c44747202fa6788b8706a
Fat1512 <letanphat15122004@gmail.com>
2025-11-23 15:55:57 +0700

    resolve conflict

---
+++ Data/scripts/postgresql_client.py
@@ -0,0 +1,98 @@
+import psycopg2
+
+
+class PostgresSQLClient:
+    def __init__(self, database, user, password, host="localhost", port="5432"):
+        self.database = database
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.conn = psycopg2.connect(
+            database=self.database,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+        )
+
+    def create_conn(self):
+        # Establishing the connection
+        conn = psycopg2.connect(
+            database=self.database,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+        )
+        return conn
+
+    def execute_query(self, query):
+        conn = self.create_conn()
+        cursor = conn.cursor()
+        cursor.execute(query)
+        print(f"Query has been executed successfully!")
+        conn.commit()
+        # Closing the connection
+        conn.close()
+
+    def cursor(self):
+        return self.conn.cursor()
+
+    def commit(self):
+        self.conn.commit()
+
+    def rollback(self):
+        self.conn.rollback()
+
+    def close(self):
+        self.conn.close()
+import psycopg2
+
+
+class PostgresSQLClient:
+    def __init__(self, database, user, password, host="localhost", port="5432"):
+        self.database = database
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.conn = psycopg2.connect(
+            database=self.database,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+        )
+
+    def create_conn(self):
+        # Establishing the connection
+        conn = psycopg2.connect(
+            database=self.database,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+        )
+        return conn
+
+    def execute_query(self, query):
+        conn = self.create_conn()
+        cursor = conn.cursor()
+        cursor.execute(query)
+        print(f"Query has been executed successfully!")
+        conn.commit()
+        # Closing the connection
+        conn.close()
+
+    def cursor(self):
+        return self.conn.cursor()
+
+    def commit(self):
+        self.conn.commit()
+
+    def rollback(self):
+        self.conn.rollback()
+
+    def close(self):
+        self.conn.close()
